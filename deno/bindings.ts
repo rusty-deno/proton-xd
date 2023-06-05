c1b4526861c338227252296e4ab589a487c7121a
kakashi-69-xd <trishiragolder69@outlook.com>
2023-06-05 23:28:09 +0530

    added bindings dialog box messages to deno

--- deno/bindings.ts
+++ deno/bindings.ts
@@ -14,9 +14,37 @@ const rust=Deno.dlopen(new URL(`../target/release/xd.${ext()}`,import.meta.url),
   init: {
     parameters: ["buffer","buffer","u16","u16","buffer"],
     result: "void"
+  },
+  message: {
+    parameters: ["i32","i32","buffer"],
+    result: "void"
+  },
+  message_default: {
+    parameters: ["buffer"],
+    result: "void"
+  },
+  hotspot: {
+    parameters: [],
+    result: "bool"
   }
 });
-
+/**
+ * message(x: i32,y: i32,txt: *const i8)
+ * 
+ * message_default(txt: *const i8)
+ * 
+ * message_hotspot()-> bool
+ * 
+ * message_icon_label(label: *const i8)
+ * 
+ * message_set_font(idx: usize,sz: i32)
+ * 
+ * message_set_hotspot(enabled: bool)
+ * 
+ * message_title(title: *const i8)
+ * 
+ * message_title_default(title: *const i8)
+ */
 
 
 const encoder=new TextEncoder;
--- deno/fltk.ts
+++ deno/fltk.ts
@@ -1,10 +1,22 @@
-import rust from './bindings.ts';
+import rust,{encoder} from './bindings.ts';
 
 
 namespace fltk {
-  
+  const lib=rust.symbols;
+  export class Dialog {
+    public static message=(x: number,y: number,txt: string)=> lib.message(x,y,encoder.encode(txt));
+
+    public static messageDefault=(txt: string)=> lib.message_default(encoder.encode(txt));
+
+    public static hotspot=lib.hotspot;
+
+    
+
+  }
 
 
+
+  const close=rust.close;
 }
 
 export default fltk;
