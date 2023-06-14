520f637f9a0f6240cb70fc7eae1c4f7a6c6f6f91
kakashi-69-xd <trishiragolder69@outlook.com>
2023-06-14 14:20:07 +0530

    fixed rgba bug

--- lib/mod.ts
+++ lib/mod.ts
@@ -5,8 +5,11 @@ import * as sc from "./screencapture.ts";
 namespace ProtonXD {
   export const XD=xd.default;
   export const Theme=xd.Theme;
+  
   export const ScreenCapturer=sc.default;
-  export const ImageBuffer=sc.ImageBuffer;
+  export const Image=sc.Image;
+  export type ImageBuffer=sc.ImageBuffer;
+  
 }
 
 
--- lib/screencapture.ts
+++ lib/screencapture.ts
@@ -1,15 +1,11 @@
+import JPEG from "npm:jpeg-js";
 import * as png from "https://deno.land/x/pngs@0.1.1/mod.ts";
-import JPEG from "https://deno.land/x/jpeg@v1.0.1/mod.ts";
 import * as lib from "../bindings/bindings.ts";
-// import * as imgs from "https://deno.land/x/get_pixels@v1.2.0/mod.ts";
-
 
 export default class ScreenCapturer {
   
 
   public static screenshot=(x: number,y: number)=> new Image(JSON.parse(lib.screenshot(x,y)));
-
-  
 }
 
 export class Image {
@@ -21,6 +17,15 @@ export class Image {
     this.height=img.height;
     this.width=img.width;
     this.bytes=img.bytes;
+
+    for(let i=0;i<this.bytes.length;i+=4) {
+      const b=this.bytes[i];
+      const r=this.bytes[i+2];
+
+      this.bytes[i]=r;
+      this.bytes[i+2]=b;
+      this.bytes[i+3]=255;
+    }
   }
 
   public png=()=> png.encode(this.bytes,this.width,this.height,{
@@ -28,7 +33,7 @@ export class Image {
     color: png.ColorType.RGBA
   });
 
-  public jpeg=()=> new Uint8Array(JPEG.encode({data: this.bytes,width: this.width,height: this.height}).data);
+  public jpeg=(quality: number=100)=> new Uint8Array(JPEG.encode({data: this.bytes,width: this.width,height: this.height},quality).data);
 }
 
 export interface ImageBuffer {
