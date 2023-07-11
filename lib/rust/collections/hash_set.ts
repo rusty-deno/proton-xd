1a54b5db3db5ee23efc226ab9f313356129ded55
kakashi-69-xd <trishiragolder69@outlook.com>
2023-07-11 23:35:13 +0530

    improved Vec

--- lib/rust/collections/hash_set.ts
+++ lib/rust/collections/hash_set.ts
@@ -4,13 +4,13 @@ import { HashMap } from "./hash_map.ts";
 
 
 export default class HashSet<T> extends Iter<T> {
-
   private map: HashMap<T,undefined>=new HashMap;
 
   constructor(...entries: T[]) {
     super();
     for(const entry of entries) this.add(entry);
   }
+  
   next(): T {
     return this[Symbol.iterator]().next().value;
   }
--- lib/rust/collections/vector.ts
+++ lib/rust/collections/vector.ts
@@ -1,62 +1,47 @@
-import Option from "../io/option.ts";
+import Option, { None } from "../io/option.ts";
 import { Enumerate } from "../iter.ts";
-import * as list from "./List.ts";
 import Clone from '../clone.ts';
 
 
-export default class Vec<T> implements Clone<Vec<T>> {
-  private readonly arr=new Array<T>;
-  
+export default class Vec<T> extends Array<T> implements Clone<Vec<T>> {
+
   constructor(...elements: T[]) {
-    this.arr.push(...elements);
+    super(...elements);
   }
 
-  public get length() {
-    return this.arr.length;
+  override forEach(f: (value: T,index: number,iter: this)=> void): void {
+    let i=0;
+    for(const iterator of this) f(iterator,i++,this);
   }
+
   
-  public set length(size: number) {
-    this.arr.length=size;
+  public fold(f: (prev: T,current: T,index: number)=> Option<T>|T): Option<T> {
+    return new Option(super.reduce((prev: T,current: T,index: number)=> {
+      const xd=f(prev,current,index);
+      return xd instanceof Option?xd.value!:xd;
+    }));
   }
 
   next(): T {
     return this[Symbol.iterator]().next().value;
   }
-  
-  *[Symbol.iterator](): Iterator<T,any,undefined> {
-    for(const iterator of this.arr) yield iterator;
-  }
-
-  public sort(f: (a: T, b: T)=> number) {
-    this.arr.sort(f);
-    return this;
-  }
-
-  public contains(data: T): boolean {
-    return this.arr.includes(data);
-  }
-
-  public indexOf(data: T): number {
-    return this.arr.indexOf(data);
-  }
-
-  public at(index: number): Option<T> {
-    return new Option(this.arr.at(index));
-  }
 
   public enumerate(): Enumerate<T> {
-    return this.arr.entries();
-  }
-  
-  public toArray(): T[] {
-    return structuredClone(this.arr);
+    return super.entries();
   }
 
   public toVec(): Vec<T> {
-    return this.clone();
+    return structuredClone(this);
+  }
+
+  public toArray(): T[] {
+    return new Array(...this);
   }
 
   public clone(): Vec<T> {
     return structuredClone(this);
   }
+  
 }
+
+
