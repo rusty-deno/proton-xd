1e37ecb7a5925b407a235257732774c3073cf491
kakashi-69-xd <trishiragolder69@outlook.com>
2023-07-24 17:53:06 +0530

    optimized linear collections

--- lib/rust/collections/linear/linked_list/mod.ts
+++ lib/rust/collections/linear/linked_list/mod.ts
@@ -5,7 +5,7 @@ export class Node<T> {
   next: Option<Node<T>>;
   
   constructor(data: T,next?: Node<T>|null) {
-  this.data=data;
-  this.next=new Option(next);
+    this.data=data;
+    this.next=new Option(next);
   }
 }
