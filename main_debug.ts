fb81c1174a485b0b2e24e389443e0e10475057af
kakashi-69-xd <trishiragolder69@outlook.com>
2023-07-29 12:12:08 +0530

    fixed path related bug

--- main_debug.ts
+++ main_debug.ts
@@ -1,30 +1,7 @@
 // deno-lint-ignore-file
-// import ProtonXD from "./mod.ts";
-
-import { readToStringSync } from "./lib/rust/fs/mod.ts";
-
-// ProtonXD.XD.instantiate("<html><body>hello wrld</body></html>",{
-//     title: "my-app"
-// });
-
-// import sum from "npm:hash-sum";
+import ProtonXD from "./mod.ts";
 
 
-// const set=new Set;
-
-
-// for(let i=1;i<=100;i++) {
-//     let x=Number.parseInt("0x"+sum({i}));
-//     set.add(x);
-// }
-
-
-
-
-
-const prototype=readToStringSync("./bindings/bindings.prototype.json").or("{}").trim();
-
-
-
-
-console.log(prototype.substring(1,prototype.length-1));
+ProtonXD.XD.instantiate("<html><body>hello wrld</body></html>",{
+    title: "my-app"
+});
