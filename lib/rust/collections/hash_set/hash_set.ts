3ee9dd9d699138a61779d5f93a773bb7eef8b263
kakashi-69-xd <trishiragolder69outlook.com>
2023-08-02 21:02:10 +0530

    improved hash_set

--- lib/rust/collections/hash_set/hash_set.ts
+++ lib/rust/collections/hash_set/hash_set.ts
@@ -1,14 +1,26 @@
 import {Iter} from "../iter.ts";
-import {HashMap} from "../hash_map/hash_map.ts";
+import { HasherFn,HashMap } from '../hash_map/mod.ts';
 
 
 
 export class HashSet<T> extends Iter<T> {
-  private map: HashMap<T,null>=new HashMap;
+  private map=new HashMap<T,null>();
 
   constructor(...entries: T[]) {
     super();
     for(const entry of entries) this.add(entry);
+  }
+
+  public static withHasher<T>(fn: HasherFn<T>,...entries: T[]) {
+    const set=new HashSet(...entries);
+    set.map.hasher=fn;
+    return set;
+  }
+  
+  public static fromIter<T>(entries: Iterable<T>) {
+    const set=new Set<T>;
+    for(const entry of entries) set.add(entry);
+    return set;
   }
   
   next(): T {
@@ -19,7 +31,7 @@ export class HashSet<T> extends Iter<T> {
     for(const entry of this.map) yield entry[0];
   }
 
-  public add(data: T) {
+  public add(data: T,) {
     this.map.set(data,null);
   }
   
