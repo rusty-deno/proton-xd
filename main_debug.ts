f57c33b6b4ce8c50408c5e1b69aa1dd3075abfcb
kakashi-69-xd <trishiragolder69outlook.com>
2023-08-03 01:15:38 +0530

    xd

--- main_debug.ts
+++ main_debug.ts
@@ -1,17 +1,8 @@
 // deno-lint-ignore-file
-// import ProtonXD from "./mod.ts";
-import { HashMap } from './lib/rust/collections/hash_map/hash_map.ts';
+import ProtonXD from "./mod.ts";
 
 
-// ProtonXD.XD.instantiate(`<html><body style="color: #ff00ff;">hello wrld</body></html>`,{
-//     title: "my-app"
-// });
-
-const map=new HashMap(["xd",100],["gg",69]);
-
-console.log(map.entries());
-
-map.set("xd",69);
-
-console.log(map.entries());
+ProtonXD.XD.instantiate(`<html><body style="color: #ff00ff;">hello wrld</body></html>`,{
+  title: "my-app"
+});
 
