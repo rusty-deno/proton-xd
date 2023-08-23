1ca8f5e16484e72d68854c20c309650b4066dd27
kakashi-69-xd <trishiragolder69@outlook.com>
2023-08-23 22:15:15 +0530

    modified screencapturer

--- lib/screencapture/screencapture.ts
+++ lib/screencapture/screencapture.ts
@@ -1,13 +1,13 @@
 import * as lib from "../../bindings/bindings.ts";
-import { Image } from "./image.ts";
+import { ImageBuffer } from "./image.ts";
 
 
-export async function screenshot(x: number,y: number,delay=0): Promise<Image> {
-  return new Image(JSON.parse(await lib.screenshot(x,y,delay/1000)));
+export async function screenshot(x: number,y: number,delay=0): Promise<ImageBuffer> {
+  return new ImageBuffer(JSON.parse(await lib.screenshot(x,y,delay/1000)));
 }
 
-export function screenshotSync(x: number,y: number,delay=0): Image{
-  return new Image(JSON.parse(lib.screenshot_sync(x,y,delay/1000)));
+export function screenshotSync(x: number,y: number,delay=0): ImageBuffer {
+  return new ImageBuffer(JSON.parse(lib.screenshot_sync(x,y,delay/1000)));
 }
 
 
