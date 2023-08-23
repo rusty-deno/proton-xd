16683484456238f890b39911e18d95f9d188f0c7
kakashi-69-xd <trishiragolder69@outlook.com>
2023-08-23 22:14:56 +0530

    improved ImageBuffer

--- lib/screencapture/image.ts
+++ lib/screencapture/image.ts
@@ -2,24 +2,30 @@ import {instantiate,convert} from "../../bindings/encoder.generated.js";
 
 await instantiate();
 
-export interface ImageBuffer {
-  height: number;
-  width: number;
-  bytes: Uint8Array;
-}
 
-export class Image {
+export type RawImage={
+  height: number;
+  width: number
+  bytes: Uint8Array;
+};
+
+/**
+ * This is an image represented by a width and height and a container of channel data.
+ * It provides direct access to its pixels and various convertions
+ */
+export class ImageBuffer {
   public width: number;
   public height: number;
-  public readonly bytes: Uint8Array;
+  public bytes: Uint8Array;
 
-  constructor(img: ImageBuffer) {
+  constructor(img: RawImage) {
+    this.bytes=img.bytes;
     this.height=img.height;
     this.width=img.width;
-    this.bytes=img.bytes;
   }
   
   public png=(): Uint8Array=> convert(this.bytes,this.height,this.width,"image/png",100);
   
   public jpeg=(quality=100): Uint8Array=> convert(this.bytes,this.height,this.width,"image/jpeg",quality);
 }
+
