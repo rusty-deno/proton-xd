11cbd71da46a0cb4a511ee42ad3d661e4add8267
kakashi-69-xd <trishiragolder69@outlook.com>
2023-09-11 14:46:15 +0530

    refactored import-exports

--- lib/std/net/macros/routes.ts
+++ lib/std/net/macros/routes.ts
@@ -1,59 +1,72 @@
 import { Server } from '../server.ts';
-import { RouteMethod } from '../types.ts';
+import { RouteHandlerDescriptor,Route } from '../types/mod.ts';
+
 
 export type Name=string|symbol;
 
-export function GET(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function GET(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.get(route,descriptor.value!);
   };
 }
 
-export function HEAD(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function HEAD(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.head(route,descriptor.value!);
   };
 }
 
-export function POST(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function POST(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.post(route,descriptor.value!);
   };
 }
 
-export function PUT(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function PUT(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.put(route,descriptor.value!);
   };
 }
 
-export function DELETE(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function DELETE(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.delete(route,descriptor.value!);
   };
 }
 
-export function CONNECT(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function CONNECT(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.connect(route,descriptor.value!);
   };
 }
 
-export function OPTIONS(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function OPTIONS(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.options(route,descriptor.value!);
   };
 }
 
-export function TRACE(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function TRACE(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.trace(route,descriptor.value!);
   };
 }
 
-export function PATCH(route: string) {
-  return function(_this: Server,_name: Name,descriptor: RouteMethod) {
+export function PATCH(route: Route) {
+  return function(_this: Server,_name: Name,descriptor: RouteHandlerDescriptor) {
     _this.patch(route,descriptor.value!);
   };
 }
+
+
+class XD extends Server {
+
+
+  @GET("/")
+  private xd(_req: Request) {
+    return new Response;
+  }
+}
+
+const _xd=new XD;
 
--- lib/std/net/server.ts
+++ lib/std/net/server.ts
@@ -1,49 +1,57 @@
 // deno-lint-ignore-file
 import { $unimplemented } from "../declarative-macros/mod.ts";
-import { RouteCallback } from "./types.ts";
+import { Handler,Route,Method } from "./types/server.ts";
+import { HashMap } from "../mod.ts";
 
 
 
 export class Server {
+  private routes=new HashMap<string,Handler>();
+
+
+  /**
+   * Adds a route to the {@linkcode Server} with specified {@linkcode Route}, {@linkcode Method}
+   */
+  public addRoute(route: Route,method: Method,handler: Handler) {
+    $unimplemented();
+  }
   
-  public get(route: string,f: RouteCallback) {
+  public get(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public head(route: string,f: RouteCallback) {
+  public head(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public post(route: string,f: RouteCallback) {
+  public post(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public put(route: string,f: RouteCallback) {
+  public put(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public delete(route: string,f: RouteCallback) {
+  public delete(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public connect(route: string,f: RouteCallback) {
+  public connect(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public options(route: string,f: RouteCallback) {
+  public options(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public trace(route: string,f: RouteCallback) {
+  public trace(route: Route,f: Handler) {
     $unimplemented();
   }
 
-  public patch(route: string,f: RouteCallback) {
+  public patch(route: Route,f: Handler) {
     $unimplemented();
   }
 }
-
-
 
 
 
