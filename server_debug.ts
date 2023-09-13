8278434a99dfd265092f95cc5de40dafc4d2d9a0
kakashi-69-xd <trishiragolder69@outlook.com>
2023-09-13 23:59:57 +0530

    .

--- server_debug.ts
+++ server_debug.ts
@@ -1,23 +1,24 @@
-
-
 import { Server } from "./lib/std/net/mod.ts";
+import { GET } from './lib/std/net/macros/routes.ts';
+import { HashMap } from "./lib/std/collections/hash_map/hash_map.ts";
+import { Handler,Method,Route } from "./lib/std/net/types/server.ts";
 
 
-// const _s=Deno.serve({},(req)=> {
-//   console.log(req);
-//   return new Response;
-// });
+class Main extends Server {
+  protected _routes=new HashMap<`${Method}${Route}`,Handler>();
+
+  @GET("/")
+  private _root() {
+    return new Response("xd");
+  }
+
+  public static main(_args: string[]): void {
+    const server=new Main;
+    server.init();
+  }
+}
 
 
-const server=new Server({ port: 6969 });
-
-server.get("/",()=> {
-  return new Response("xd");
-});
-
-
-server.init();
-
-
+Main.main([]);
 
 
