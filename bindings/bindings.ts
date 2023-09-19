fc8ee385f105bcb939795fb48735c670a53141fb
kakashi-69-xd <trishiragolder69@outlook.com>
2023-09-20 00:37:04 +0530

    implemented prototypes of getters

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,5 +1,5 @@
-const encoder=new TextEncoder();
-const decoder=new TextDecoder();
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 
 function decode(buffer: Uint8Array): string {
   return decoder.decode(buffer);
@@ -22,29 +22,29 @@ function getExt() {
 
 // deno-lint-ignore no-explicit-any
 function readPointer(v: any): Uint8Array {
-  const ptr=new Deno.UnsafePointerView(v);
-  const lengthBe=new Uint8Array(4);
-  const view=new DataView(lengthBe.buffer);
+  const ptr = new Deno.UnsafePointerView(v);
+  const lengthBe = new Uint8Array(4);
+  const view = new DataView(lengthBe.buffer);
   ptr.copyInto(lengthBe, 0);
-  const buf=new Uint8Array(view.getUint32(0));
+  const buf = new Uint8Array(view.getUint32(0));
   ptr.copyInto(buf, 4);
   return buf;
 }
 
-const url=new URL(`./bin/${Deno.build.target}.${getExt()}`, import.meta.url);
+const url = new URL(`./bin/${Deno.build.target}.${getExt()}`, import.meta.url);
 
-let uri=url.pathname;
+let uri = url.pathname;
 
 // https://docs.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibrarya#parameters
 if (Deno.build.os === "windows") {
-  uri=uri.replace(/\//g, "\\");
+  uri = uri.replace(/\//g, "\\");
   // Remove leading slash
   if (uri.startsWith("\\")) {
-    uri=uri.slice(1);
+    uri = uri.slice(1);
   }
 }
 
-export const lib=Deno.dlopen(uri, {
+export const lib = Deno.dlopen(uri, {
   alert: {
     parameters: ["buffer", "usize", "buffer", "usize", "u8"],
     result: "void",
@@ -55,9 +55,30 @@ export const lib=Deno.dlopen(uri, {
     result: "void",
     nonblocking: false,
   },
-  init: {
-    parameters: ["buffer", "usize", "buffer", "usize"],
-    result: "void",
+  available_monitors: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
+  current_monitor: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
+  cursor_position: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
+  fullscreen: { parameters: ["usize"], result: "void", nonblocking: false },
+  inner_position: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
+  monitor_from_point: {
+    parameters: ["usize", "f64", "f64"],
+    result: "buffer",
     nonblocking: false,
   },
   open: {
@@ -70,9 +91,26 @@ export const lib=Deno.dlopen(uri, {
     result: "buffer",
     nonblocking: false,
   },
+  outer_position: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
+  outer_size: { parameters: ["usize"], result: "buffer", nonblocking: false },
   park: { parameters: [], result: "void", nonblocking: false },
   park_timeout: { parameters: ["f64"], result: "void", nonblocking: false },
+  primary_monitor: {
+    parameters: ["usize"],
+    result: "buffer",
+    nonblocking: false,
+  },
   read_clipboard: { parameters: [], result: "buffer", nonblocking: false },
+  request_redraw: { parameters: ["usize"], result: "void", nonblocking: false },
+  request_user_attention: {
+    parameters: ["usize", "buffer", "usize"],
+    result: "void",
+    nonblocking: false,
+  },
   save: {
     parameters: ["buffer", "usize"],
     result: "buffer",
@@ -83,6 +121,7 @@ export const lib=Deno.dlopen(uri, {
     result: "buffer",
     nonblocking: false,
   },
+  scale_factor: { parameters: ["usize"], result: "f64", nonblocking: false },
   screenshot: {
     parameters: ["i32", "i32", "f32"],
     result: "buffer",
@@ -94,6 +133,7 @@ export const lib=Deno.dlopen(uri, {
     nonblocking: false,
   },
   sleep: { parameters: ["f32"], result: "void", nonblocking: false },
+  title: { parameters: ["usize"], result: "buffer", nonblocking: false },
   write_to_clipboard: {
     parameters: ["buffer", "usize"],
     result: "void",
@@ -117,12 +157,23 @@ export const lib=Deno.dlopen(uri, {
     "parameters": ["buffer", "usize", "u32", "u32", "u8", "u8"],
     "result": "buffer",
   },
+  "init": {
+    "parameters": ["buffer", "buffer", "function"],
+    "result": "void",
+  },
+  "set_cursor_visible": {
+    "parameters": ["pointer"],
+    "result": "void",
+  },
 });
-const { symbols }=lib;
+const { symbols } = lib;
 addEventListener("unload", () => {
   lib.close();
 });
-export type FileDialogOptions={
+export type AttentionType =
+  | "Critical"
+  | "Informational";
+export type FileDialogOptions = {
   location: string;
   filename: string;
   typ: FileOpenerType;
@@ -131,28 +182,40 @@ export type FileOpenerType =
   | "SingleFile"
   | "SingleDir"
   | "MultipleFile";
-export type Header={
+export type Header = {
   name: string;
   value: string;
 };
-export type Position={
+export type MonitorData = {
+  name: string | undefined | null;
+  position: Position;
+  scale_factor: number;
+  size: Size;
+  modes: Array<VidMode>;
+};
+export type Position = {
   x: number;
   y: number;
 };
-export type Rgba={
+export type Rgba = {
   r: number;
   g: number;
   b: number;
   a: number;
 };
-export type Size={
+export type Size = {
   height: number;
   width: number;
 };
 export type Theme =
   | "Light"
   | "Dark";
-export type WebViewAttrs={
+export type VidMode = {
+  size: Size;
+  bit_depth: number;
+  refresh_rate: number;
+};
+export type WebViewAttrs = {
   user_agent: string | undefined | null;
   visible: boolean;
   transparent: boolean;
@@ -169,7 +232,7 @@ export type WebViewAttrs={
   url: string | undefined | null;
   headers: Array<Header> | undefined | null;
 };
-export type WindowAttrs={
+export type WindowAttrs = {
   inner_size: Size | undefined | null;
   min_height: number | undefined | null;
   max_height: number | undefined | null;
@@ -194,111 +257,166 @@ export type WindowAttrs={
   position: Position | undefined | null;
 };
 export function alert(a0: string, a1: string, a2: number) {
-  const a0_buf=encode(a0);
-  const a1_buf=encode(a1);
+  const a0_buf = encode(a0);
+  const a1_buf = encode(a1);
 
-  const rawResult=symbols.alert(
+  const rawResult = symbols.alert(
     a0_buf,
     a0_buf.byteLength,
     a1_buf,
     a1_buf.byteLength,
     a2,
   );
-  const result=rawResult;
+  const result = rawResult;
   return result;
 }
 export function alert_sync(a0: string, a1: string, a2: number) {
-  const a0_buf=encode(a0);
-  const a1_buf=encode(a1);
+  const a0_buf = encode(a0);
+  const a1_buf = encode(a1);
 
-  const rawResult=symbols.alert_sync(
+  const rawResult = symbols.alert_sync(
     a0_buf,
     a0_buf.byteLength,
     a1_buf,
     a1_buf.byteLength,
     a2,
   );
-  const result=rawResult;
+  const result = rawResult;
   return result;
 }
-export function init(a0: string, a1: string) {
-  const a0_buf=encode(a0);
-  const a1_buf=encode(a1);
-
-  const rawResult=symbols.init(
-    a0_buf,
-    a0_buf.byteLength,
-    a1_buf,
-    a1_buf.byteLength,
-  );
-  const result=rawResult;
+export function available_monitors(a0: bigint) {
+  const rawResult = symbols.available_monitors(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function current_monitor(a0: bigint) {
+  const rawResult = symbols.current_monitor(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function cursor_position(a0: bigint) {
+  const rawResult = symbols.cursor_position(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function fullscreen(a0: bigint) {
+  const rawResult = symbols.fullscreen(a0);
+  const result = rawResult;
   return result;
 }
-export async function open(a0: string) {
-  const a0_buf=encode(a0);
+export function inner_position(a0: bigint) {
+  const rawResult = symbols.inner_position(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function monitor_from_point(a0: bigint, a1: number, a2: number) {
+  const rawResult = symbols.monitor_from_point(a0, a1, a2);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function open(a0: string) {
+  const a0_buf = encode(a0);
 
-  const rawResult=symbols.open(a0_buf, a0_buf.byteLength);
-  const result=rawResult.then(readPointer);
-  const buffer=await result;
-  return decode(buffer);
+  const rawResult = symbols.open(a0_buf, a0_buf.byteLength);
+  const result = rawResult.then(readPointer);
+  return result.then(decode);
 }
 export function open_sync(a0: string) {
-  const a0_buf=encode(a0);
+  const a0_buf = encode(a0);
 
-  const rawResult=symbols.open_sync(a0_buf, a0_buf.byteLength);
-  const result=readPointer(rawResult);
+  const rawResult = symbols.open_sync(a0_buf, a0_buf.byteLength);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function outer_position(a0: bigint) {
+  const rawResult = symbols.outer_position(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function outer_size(a0: bigint) {
+  const rawResult = symbols.outer_size(a0);
+  const result = readPointer(rawResult);
   return decode(result);
 }
 export function park() {
-  const rawResult=symbols.park();
-  const result=rawResult;
+  const rawResult = symbols.park();
+  const result = rawResult;
   return result;
 }
 export function park_timeout(a0: number) {
-  const rawResult=symbols.park_timeout(a0);
-  const result=rawResult;
+  const rawResult = symbols.park_timeout(a0);
+  const result = rawResult;
   return result;
 }
-export function read_clipboard() {
-  const rawResult=symbols.read_clipboard();
-  const result=readPointer(rawResult);
+export function primary_monitor(a0: bigint) {
+  const rawResult = symbols.primary_monitor(a0);
+  const result = readPointer(rawResult);
   return decode(result);
 }
-export async function save(a0: string) {
-  const a0_buf=encode(a0);
+export function read_clipboard() {
+  const rawResult = symbols.read_clipboard();
+  const result = readPointer(rawResult);
+  return decode(result);
+}
+export function request_redraw(a0: bigint) {
+  const rawResult = symbols.request_redraw(a0);
+  const result = rawResult;
+  return result;
+}
+export function request_user_attention(a0: bigint, a1: string) {
+  const a1_buf = encode(a1);
 
-  const rawResult=symbols.save(a0_buf, a0_buf.byteLength);
-  const result=rawResult.then(readPointer);
-  const buffer=await result;
-  return decode(buffer);
+  const rawResult = symbols.request_user_attention(
+    a0,
+    a1_buf,
+    a1_buf.byteLength,
+  );
+  const result = rawResult;
+  return result;
+}
+export function save(a0: string) {
+  const a0_buf = encode(a0);
+
+  const rawResult = symbols.save(a0_buf, a0_buf.byteLength);
+  const result = rawResult.then(readPointer);
+  return result.then(decode);
 }
 export function save_sync(a0: string) {
-  const a0_buf=encode(a0);
+  const a0_buf = encode(a0);
 
-  const rawResult=symbols.save_sync(a0_buf, a0_buf.byteLength);
-  const result=readPointer(rawResult);
+  const rawResult = symbols.save_sync(a0_buf, a0_buf.byteLength);
+  const result = readPointer(rawResult);
   return decode(result);
 }
-export async function screenshot(a0: number, a1: number, a2: number) {
-  const rawResult=symbols.screenshot(a0, a1, a2);
-  const result=rawResult.then(readPointer);
-  const buffer=await result;
-  return decode(buffer);
+export function scale_factor(a0: bigint) {
+  const rawResult = symbols.scale_factor(a0);
+  const result = rawResult;
+  return result;
+}
+export function screenshot(a0: number, a1: number, a2: number) {
+  const rawResult = symbols.screenshot(a0, a1, a2);
+  const result = rawResult.then(readPointer);
+  return result.then(decode);
 }
 export function screenshot_sync(a0: number, a1: number, a2: number) {
-  const rawResult=symbols.screenshot_sync(a0, a1, a2);
-  const result=readPointer(rawResult);
+  const rawResult = symbols.screenshot_sync(a0, a1, a2);
+  const result = readPointer(rawResult);
   return decode(result);
 }
 export function sleep(a0: number) {
-  const rawResult=symbols.sleep(a0);
-  const result=rawResult;
+  const rawResult = symbols.sleep(a0);
+  const result = rawResult;
   return result;
 }
+export function title(a0: bigint) {
+  const rawResult = symbols.title(a0);
+  const result = readPointer(rawResult);
+  return decode(result);
+}
 export function write_to_clipboard(a0: string) {
-  const a0_buf=encode(a0);
+  const a0_buf = encode(a0);
 
-  const rawResult=symbols.write_to_clipboard(a0_buf, a0_buf.byteLength);
-  const result=rawResult;
+  const rawResult = symbols.write_to_clipboard(a0_buf, a0_buf.byteLength);
+  const result = rawResult;
   return result;
 }
