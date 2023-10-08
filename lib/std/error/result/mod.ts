1f63f37ecced506fa0fb1c2ccdabf5b8fee6a2f1
kakashi-69-xd <trishiragolder69@outlook.com>
2023-10-08 08:17:21 +0530

    fixed net

--- lib/std/error/result/mod.ts
+++ lib/std/error/result/mod.ts
@@ -1,7 +1,6 @@
 export * from "./result.ts";
 export * from "./macros.ts";
-
-
+export * from "./async_result.ts";
 
 
 export type Res<T,E>={ ok: T }|{ err: E };
--- lib/std/net/macros/fetch.ts
+++ lib/std/net/macros/fetch.ts
@@ -1,11 +1,11 @@
-import { $result,Result } from "../../error/result/mod.ts";
+import { $result,AsyncResult } from "../../error/result/mod.ts";
 import { HttpError } from "../error.ts";
 
 
 /**
  * An improved version of the native {@linkcode fetch} api
  */
-export function $fetch(inp: string|URL|Request,init?: RequestInit): Promise<Result<Response,HttpError>> {
+export function $fetch(inp: string|URL|Request,init?: RequestInit): AsyncResult<Response,HttpError> {
   return $result(async ()=> {
     const res=await fetch(inp,init);
     if(!res.ok) throw new HttpError(res);
