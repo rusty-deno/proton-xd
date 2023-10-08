173c373e7a20a6472bd970d8738ebcc12f5e8260
kakashi-69-xd <trishiragolder69@outlook.com>
2023-10-08 08:19:13 +0530

    fixed error module sys

--- lib/std/error/mod.ts
+++ lib/std/error/mod.ts
@@ -2,6 +2,7 @@ export * as option from "./option/mod.ts";
 export * as result from "./result/mod.ts";
 
 export * from "./result/result.ts";
+export * from "./result/async_result.ts";
 export * from "./option/option.ts";
 export * from "./exception.ts";
 
