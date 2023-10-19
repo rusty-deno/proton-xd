a1da4320fee2fc4c425866b2f4a67709e5483c43
kakashi-69-xd <trishiragolder69@outlook.com>
2023-10-19 22:53:25 +0530

    improved List

--- lib/std/collections/linear/List.ts
+++ lib/std/collections/linear/List.ts
@@ -1,10 +1,26 @@
 import { Option } from "../../mod.ts";
+import { LinkedList } from './linked_list/linked_list.ts';
+import { Vec } from './vector.ts';
 
 
 
 export abstract class List<T> implements Iterable<T> {
   abstract [Symbol.iterator](): Iterator<T>;
   abstract next(): T;
+
+  public static fromIter<T>(iter: Iterable<T>) {
+    return new class extends List<T> {
+      next(): T {
+        return this[Symbol.iterator]().next().value;
+      }
+      [Symbol.iterator](): Iterator<T> {
+        return iter[Symbol.iterator]();
+      }
+    };
+  }
+
+
+
   /**
    * @param {number} index 
    * @returns {Option<T>}
@@ -14,9 +30,10 @@ export abstract class List<T> implements Iterable<T> {
     return new Option(Object.values(this)[index]);
   }
   
-  public *enumerate(): Iterable<[T,number]> {
+  /** Enumerates the List. */
+  public *enumerate(): Iterable<[number,T]> {
     let i=0;
-    for(const element of this) yield [element,i++];
+    for(const element of this) yield [i++,element];
   }
 
   /**
@@ -25,7 +42,7 @@ export abstract class List<T> implements Iterable<T> {
    * @description improvement todo
    */
   public indexOf(data: T): number {
-    for(const [element,i] of this.enumerate()) if(Object.is(data,element)) return i;
+    for(const [i,element] of this.enumerate()) if(Object.is(data,element)) return i;
     return -1;
   }
 
@@ -33,4 +50,18 @@ export abstract class List<T> implements Iterable<T> {
     return Boolean(this.indexOf(data)+1);
   }
   
+  public forEach(f: (element: T,index: number,iter: List<T>)=> void) {
+    for(const [i,element] of this.enumerate()) f(element,i,this);
+  }
+
+  public map<U>(f: (element: T,index: number,iter: List<T>)=> U): List<U> {
+    const map=new LinkedList<U>();
+    for(const [i,element] of this.enumerate()) map.pushBack(f(element,i,this));
+
+    return List.fromIter(map);
+  }
+
+  public toVec() {
+    return Vec.fromIter(this);
+  }
 }
