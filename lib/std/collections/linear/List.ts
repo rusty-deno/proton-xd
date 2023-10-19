69f4269e13313807ecc00d09eb1752b319838dbd
kakashi-69-xd <trishiragolder69@outlook.com>
2023-10-19 22:35:01 +0530

    improved List

--- lib/std/collections/linear/List.ts
+++ lib/std/collections/linear/List.ts
@@ -2,7 +2,7 @@ import { Option } from "../../mod.ts";
 
 
 
-export abstract class List<T> {
+export abstract class List<T> implements Iterable<T> {
   abstract [Symbol.iterator](): Iterator<T>;
   abstract next(): T;
   /**
@@ -13,15 +13,22 @@ export abstract class List<T> {
   public at(index: number): Option<T> {
     return new Option(Object.values(this)[index]);
   }
-      
+  
+  public *enumerate(): Iterable<[T,number]> {
+    let i=0;
+    for(const element of this) yield [element,i++];
+  }
+
   /**
    * @param {T} data 
    * @returns {number}
    * @description improvement todo
    */
   public indexOf(data: T): number {
-    return Object.values(this).indexOf(data);
+    for(const [element,i] of this.enumerate()) if(Object.is(data,element)) return i;
+    return -1;
   }
+
   public contains(data: T) {
     return Boolean(this.indexOf(data)+1);
   }
