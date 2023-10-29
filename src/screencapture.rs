78051ebd48b1e7cb6fc9224038f7a32f398336ea
kakashi-69-xd <trishiragolder69@outlook.com>
2023-10-30 04:29:31 +0530

    improved

--- src/screencapture.rs
+++ src/screencapture.rs
@@ -6,21 +6,22 @@ use deno_bindgen::deno_bindgen;
 
 
 #[deno_bindgen(non_blocking)]
-pub fn screenshot(x: i32,y: i32,delay: f32,ptr: usize)-> String {
+pub fn screenshot(x: i32,y: i32,delay: f32,ptr: usize)-> Vec<u8> {
   ss(x,y,delay,ptr as *mut u32)
 }
 
-#[deno_bindgen(non_blocking)]
-pub fn screenshot_sync(x: i32,y: i32,delay: f32,ptr: usize)-> String {
-  ss(x,y,delay,ptr as *mut u32)
+#[deno_bindgen]
+pub fn screenshot_sync(x: i32,y: i32,delay: f32,ptr: usize)-> Vec<u8> {
+  ss(x,y,delay,ptr as *mut u32);
+  vec![]
 }
 
-pub fn ss(x: i32,y: i32,delay: f32,ptr: *mut u32)-> String {
+pub fn ss(x: i32,y: i32,delay: f32,ptr: *mut u32)-> Vec<u8> {
   thread::sleep(Duration::from_secs_f32(delay));
   let img=screenshoter::ScreenCapturer::from_point(x,y).unwrap().capture().unwrap();
   unsafe {
     (*ptr)=img.height;
     (*ptr.offset(1))=img.width;
-    String::from_utf8_unchecked(img.bytes)
+    img.bytes
   }
 }
