45a3a316cb509dc0baef5b8e96f0ddc93dc94500
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-05 08:11:56 +0530

    improved

--- image/build.ts
+++ image/build.ts
@@ -3,6 +3,8 @@ type Cmd=[string,string[]];
 
 const COMPILE: Cmd=["cargo",["build","--release","--target","wasm32-unknown-unknown"]];
 const BINDGEN: Cmd=["wasm-bindgen",["--target","deno","--weak-refs","--reference-types","--debug","--out-dir","lib","target/wasm32-unknown-unknown/release/rs_lib.wasm"]];
+const CLEAN: Cmd=["cargo",["clean"]];
+
 
 const run=([cmd,args]: Cmd)=> new Deno.Command(cmd,{
   args,
@@ -30,10 +32,26 @@ async function compile() {
   return (await process.status).code;
 }
 
+async function clean() {
+  const status=await run(CLEAN).status;
+  const cwd=Deno.cwd();
+
+  Deno.chdir(new URL("./lib/",import.meta.url));
+  for await(const file of Deno.readDir("./")) {
+    Deno.remove(file.name);
+  }
+
+  Deno.chdir(cwd);
+  return status.code;
+}
+
+
+
 async function main() {
   switch(Deno.args[0]) {
     case "compile": return await compile();
     case "bindgen": return await bindgen();
+    case "clean": return clean();
     case "build": default: return await compile().then(bindgen);
   }
 }
