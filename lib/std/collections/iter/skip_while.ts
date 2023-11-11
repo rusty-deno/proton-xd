0b99b88e59b4e731509dd761ecb0d75b30c5fbe0
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-12 02:57:33 +0530

    implemented skip_while.ts

---
+++ lib/std/collections/iter/skip_while.ts
@@ -0,0 +1,27 @@
+import { Iter } from './iter.ts';
+import { LinkedList } from '../linear/linked_list/linked_list.ts';
+import { Fn } from "../../types.ts";
+
+
+export class SkipWhile<T> extends Iter<T> {
+
+  constructor(iter: LinkedList<T>,private f: Fn<[T],boolean>) {
+    super(iter);
+  }
+
+  override *[Symbol.iterator](): Iterator<T> {
+    for(const iter of this) {
+      if(this.f(iter)) continue;
+      yield iter;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
