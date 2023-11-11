bd96d8be568d046d6fef8f6f447563dd080c5e56
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-12 02:50:36 +0530

    implemented rev

---
+++ lib/std/collections/iter/rev.ts
@@ -0,0 +1,11 @@
+import { Iter } from './iter.ts';
+
+
+export class RevIter<T> extends Iter<T> {
+  override *[Symbol.iterator](): Iterator<T> {
+    for(let iter=this.iter.back.value;iter&&iter.prev.deref()?.value;iter=iter.prev.deref()?.value) yield iter.data;
+  }
+}
+
+
+
