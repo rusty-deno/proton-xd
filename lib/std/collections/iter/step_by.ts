f1d041b1b2afd5bf08fd1700d6cde38c0c4a20b4
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-12 03:21:25 +0530

    implemented step_by

---
+++ lib/std/collections/iter/step_by.ts
@@ -0,0 +1,21 @@
+import { Iter } from './iter.ts';
+import { LinkedList } from '../linear/linked_list/linked_list.ts';
+
+
+export class StepBy<T> extends Iter<T> {
+  constructor(iter: LinkedList<T>,private _step: number) {
+    super(iter);
+  }
+
+  override *[Symbol.iterator](): Iterator<T> {
+    let i=0;
+    for(const iter of this) if(i++%this._step==0) yield iter;
+  }
+}
+
+
+
+
+
+
+
