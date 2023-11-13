d40fdddc87c6678522c4caf95e1d0760d432f934
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-13 17:56:15 +0530

    implemented map_while

---
+++ lib/std/collections/iterator/map_while.ts
@@ -0,0 +1,20 @@
+import { IterableTrait,Fn } from "./mod.ts";
+import { None,Option } from "../../mod.ts";
+
+
+export class IterMapWhile<T,U> extends IterableTrait<U> {
+  constructor(private _iter: Iterable<T>,private f: Fn<[T,number],Option<U>|U|None>) {
+    super();
+  }
+
+  override *[Symbol.iterator](): Iterator<U> {
+    let i=0;
+    for(const iter of this._iter) {
+      const _res=this.f(iter,i++);
+      const res=_res instanceof Option?_res.value:_res;
+      if(res==null) break;
+      
+      yield res;
+    }
+  }
+}
