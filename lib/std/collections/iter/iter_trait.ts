fe424b68e8e933f74cbddfd6b46cd6b61c25cc36
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-14 16:19:37 +0530

    implemented iterator_trait

--- lib/std/collections/iter/iter_trait.ts
+++ lib/std/collections/iter/iter_trait.ts
@@ -1,58 +1,28 @@
-import { Rev } from "./rev.ts";
 import { Chain } from "./mod.ts";
 import { Skip } from "./skip.ts";
 import { Take } from './take.ts';
 import { FlatMap } from "./mod.ts";
-import { IterMap } from "./map.ts";
 import { Fn } from "../../types.ts";
 import { Filter } from "./filter.ts";
 import { StepBy } from "./step_by.ts";
 import { Flatten } from "./flatten.ts";
-import { Inspect } from "./inspect.ts";
-import { Enumerate } from './enumerate.ts';
 import { SkipWhile } from "./skip_while.ts";
 import { TakeWhile } from "./take_while.ts";
 import { IterMapWhile } from "./map_while.ts";
-import { $unimplemented } from "../../mod.ts";
+import { $unimplemented } from "../../mod.ts";// unimplemented
 import { Option,Some,None } from "../../mod.ts";
+import { IteratorTrait } from './iterator_trait.ts';
 
 
 
 
-export abstract class IterTrait<T> implements Iterable<T> {
-  abstract [Symbol.iterator](): Iterator<T>;
-
-  public next() {
-    const next=this[Symbol.iterator]().next().value;
-    return new Option<T>(next==null?null:next);
-  }
-
-  public all(f: Fn<[element: T],boolean>) {
-    for(const iter of this) if(!f(iter)) return false;
-
-    return true;
-  }
-
-  public any(f: Fn<[element: T],boolean>) {
-    for(const iter of this) if(f(iter)) return true;
-
-    return false;
-  }
-
-  public asArray() {
-    return [...this];
-  }
-
+export abstract class IterTrait<T> extends IteratorTrait<T> {
   public chain(iter: Iterable<T>) {
     return new Chain(this,iter);
   }
 
   public cycle() {
     $unimplemented();
-  }
-
-  public enumerate() {
-    return new Enumerate(this);
   }
 
   public filter(f: Fn<[element: T],boolean>) {
@@ -82,25 +52,6 @@ export abstract class IterTrait<T> implements Iterable<T> {
   public static flatten<T,U extends Iterable<T>,I extends Iterable<U>>(iter: I) {
     return new Flatten<T,U,I>(iter);
   }
-
-  public fold<U>(init: U,f: Fn<[prev: U,element: T],U>) {
-    for(const iter of this) init=f(init,iter);
-    
-    return init;
-  }
-
-  public forEach(f: Fn<[element: T,index: number],void>) {
-    let i=0;
-    for(const iter of this) f(iter,i++);
-  }
-
-  public inspect(f: Fn<[element: T],void>) {
-    return new Inspect(this,f);
-  }
-  
-  public map<U>(f: Fn<[element: T,index: number],U>) {
-    return new IterMap(this,f);
-  }
   
   public mapWhile<U>(f: Fn<[element: T,index: number],Option<U>|U|None>) {
     return new IterMapWhile(this,f);
@@ -110,17 +61,6 @@ export abstract class IterTrait<T> implements Iterable<T> {
     for(const [i,element] of this.enumerate()) if(f(element)) i;
 
     return -1;
-  }
-
-  public reduce(f: Fn<[prev: T,current: T],T>): Option<T> {
-    const first=this.next();
-    if(first.value==null) return first;
-
-    return Some(this.fold(first.value,f));
-  }
-
-  public rev() {
-    return new Rev(this);
   }
 
   public rfind(f: Fn<[element: T],boolean>) {
---
+++ lib/std/collections/iter/iterator_trait.ts
@@ -0,0 +1,84 @@
+import { Enumerate, Rev } from "./mod.ts";
+import { Iter } from "./iter.ts";
+import { IterMap } from "./map.ts";
+import { Fn } from "../../types.ts";
+import { Inspect } from "./inspect.ts";
+import { Vec } from '../linear/vector.ts';
+import { Option,Some } from "../../mod.ts";
+
+
+
+
+export abstract class IteratorTrait<T> implements Iterable<T> {
+  abstract [Symbol.iterator](): Iterator<T>;
+
+  public next() {
+    return new Option<T>(this[Symbol.iterator]().next().value);
+  }
+
+  public all(f: Fn<[element: T],boolean>) {
+    for(const iter of this) if(!f(iter)) return false;
+
+    return true;
+  }
+
+  public any(f: Fn<[element: T],boolean>) {
+    for(const iter of this) if(f(iter)) return true;
+
+    return false;
+  }
+
+  public enumerate() {
+    return new Enumerate(this);
+  }
+
+  public fold<U>(init: U,f: Fn<[prev: U,element: T],U>) {
+    for(const iter of this) init=f(init,iter);
+    
+    return init;
+  }
+
+  public forEach(f: Fn<[element: T,index: number],void>) {
+    let i=0;
+    for(const iter of this) f(iter,i++);
+  }
+
+  public inspect(f: Fn<[element: T],void>) {
+    return new Inspect(this,f);
+  }
+
+  public iter() {
+    return new Iter<T>(this);
+  }
+
+  public map<U>(f: Fn<[element: T,index: number],U>) {
+    return new IterMap(this,f);
+  }
+
+  public reduce(f: Fn<[prev: T,current: T],T>): Option<T> {
+    const first=this.next();
+    if(first.value==null) return first;
+
+    return Some(this.fold(first.value,f));
+  }
+
+  public rev() {
+    return new Rev(this);
+  }
+  
+  public toArray() {
+    return Array.from(this);
+  }
+
+  public toVec() {
+    return Vec.fromIter(this);
+  }
+}
+
+
+
+
+
+
+
+
