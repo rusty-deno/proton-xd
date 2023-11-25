0140dd1768bdf2d0df5086facc475c18d1b4f211
kakashi-69-xd <trishiragolder69@outlook.com>
2023-11-25 19:38:46 +0530

    implemented Thread

--- image/rs_lib/src/thread.rs
+++ image/rs_lib/src/thread.rs
@@ -1,6 +1,6 @@
 use crate::*;
 use wasm_thread::{
-  Thread as Inner,
+  JoinHandle,
   self as thread
 };
 
@@ -8,27 +8,72 @@ use wasm_thread::{
 #[wasm_bindgen]
 #[allow(dead_code)]
 pub struct Thread {
-  inner: Inner
+  inner: JoinHandle<()>
+}
+
+impl From<JoinHandle<()>> for Thread {
+  fn from(inner: JoinHandle<()>)-> Self {
+    Thread { inner }
+  }
+}
+
+impl From<*const ()> for Thread {
+  fn from(ptr: *const ())-> Self {
+    unsafe {
+      Self {
+        inner: thread::spawn(to_fnptr(ptr))
+      }
+    }
+  }
 }
 
 #[wasm_bindgen]
 impl Thread {
   #[wasm_bindgen(constructor)]
-  pub unsafe fn new(_ptr: *const ()) {
-    unimplemented!()
+  pub unsafe fn new(ptr: *const ())-> Self {
+    ptr.into()
+  }
+
+  #[wasm_bindgen]
+  pub fn name(&self)-> String {
+    self.inner.thread().name().unwrap_or_default().to_owned()
+  }
+
+  #[wasm_bindgen]
+  pub fn unpark(&self) {
+    self.inner.thread().unpark()
+  }
+  
+  #[wasm_bindgen]
+  pub fn join(self) {
+    self.inner.join().unwrap_throw()
   }
 }
 
-impl From<Inner> for Thread {
-  fn from(inner: Inner)-> Self {
-    Thread { inner }
-  }
+#[async_fn]
+pub async fn join_async(this: Thread) {
+  this.inner.join_async().await.unwrap_throw()
 }
+
+
 
 
 #[wasm_bindgen]
 pub fn current()-> Thread {
-  thread::current().into()
+  todo!()
+}
+
+#[wasm_bindgen]
+pub unsafe fn spawn(ptr: *const ()) {
+  thread::spawn(to_fnptr(ptr));
+}
+
+#[wasm_bindgen]
+pub fn sleep(duration: f32) {
+  thread::sleep(std::time::Duration::from_secs_f32(duration));
 }
 
 
+unsafe fn to_fnptr(ptr: *const ())-> fn()-> () {
+  std::mem::transmute(ptr)
+}
