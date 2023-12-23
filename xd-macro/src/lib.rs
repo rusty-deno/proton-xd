ff61a2ab6a12b022d56a7cc66680b07e223a5306
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-23 19:33:58 +0530

    partially implemented my first ever macro in life

--- xd-macro/src/lib.rs
+++ xd-macro/src/lib.rs
@@ -1,16 +1,33 @@
 extern crate proc_macro;
 
+
+use syn::ItemFn;
+use quote::quote;
 use proc_macro::TokenStream;
 
 
-
 #[proc_macro_attribute]
-pub fn xd(attr: TokenStream,_item: TokenStream)-> TokenStream {
-  
+pub fn xd(_attr: TokenStream,input: TokenStream)-> TokenStream {
+  let func=syn::parse::<ItemFn>(input).unwrap();
 
+  let name=func.sig.ident;
+  let return_type=func.sig.output;
 
-
-
-  todo!()
+  TokenStream::from(quote! {
+    #[deno_bindgen]
+    pub fn #name (ptr: usize)#return_type {
+      
+    }
+  })
 }
 
+
+// TokenStream::from(quote! {
+//   #[no_mangle]
+//   pub extern "C" fn #name <'sym> (#(#params,) *) -> #result {
+//     fn __inner_impl #fn_generics (#fn_inputs) #fn_output #fn_block
+//     #overrides
+//     let result = __inner_impl(#(#input_idents, ) *);
+//     #transformer
+//   }
+// })
---
+++ xd-macro/tests/main.rs
@@ -0,0 +1,8 @@
+#![cfg(test)]
+use xd_macro::xd;
+use deno_bindgen_macro::deno_bindgen;
+
+#[xd]
+fn _xd(xd: &str) {
+
+}
