b4a77d2276559e79c58983ae423c6b164f76d155
kakashi-69-xd <trishiragolder69@outlook.com>
2023-12-30 12:50:22 +0530

    fixed bug

--- src/dialog/fs.rs
+++ src/dialog/fs.rs
@@ -1,9 +1,50 @@
 
+use std::mem;
+use deno_bindgen::*;
+use xd_macro::method;
+use native_dialog::FileDialog;
+use crate::exception::Exception;
 
-#[no_mangle]
-pub extern "C" fn xd() {
-  
+
+
+#[deno_bindgen]
+pub fn new_file_dialog()-> usize {
+  Box::into_raw(Box::new(FileDialog::new())) as _
 }
+
+#[method]
+pub fn add_filter(this: &mut FileDialog,description: &str,extensions: &str) {
+  let extensions=serde_json::from_str::<Box<[&str]>>(extensions).unwrap_or_throw();
+  *this=mem::take(this).add_filter(description,&extensions);
+}
+
+#[method]
+pub fn remove_all_filters(this: &mut FileDialog) {
+  *this=mem::take(this).remove_all_filters();
+}
+
+#[method]
+pub fn reset_filename(this: &mut FileDialog) {
+  *this=mem::take(this).reset_filename();
+}
+
+#[method]
+pub fn reset_location(this: &mut FileDialog) {
+  *this=mem::take(this).reset_location();
+}
+
+#[method]
+pub fn reset_owner(this: &mut FileDialog) {
+  *this=mem::take(this).reset_owner();
+}
+
+#[method]
+pub fn set_filename(this: &FileDialog,filename: &str) {
+  // this.set_filename(filename);
+}
+
+
+
 
 
 
