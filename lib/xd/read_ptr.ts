58279299365dd7085b4445dff3754e7364b09f6f
kakashi-69-xd <trishiragolder69@outlook.com>
2024-01-06 05:53:43 +0530

    implemented pointer reader

---
+++ lib/xd/read_ptr.ts
@@ -0,0 +1,30 @@
+import { symbols as allocator } from "../../bindings/bindings.ts";
+
+
+type Touple<T>=[T,T];
+type Pointer=Deno.PointerValue;
+
+export function readPtr(ptr: Pointer,len: number) {
+  return Deno.UnsafePointerView.getArrayBuffer(ptr!,len);
+}
+
+
+export function getI32Touple(ptr: Pointer): Touple<number> {
+  const arr=new Int32Array(readPtr(ptr,8));
+  const touple=[arr[0],arr[1]] satisfies Touple<number>;
+
+  allocator.free(ptr);
+  return touple;
+}
+
+
+export function getU32Touple(ptr: Pointer): Touple<number> {
+  const arr=new Uint32Array(readPtr(ptr,8));
+  const touple=[arr[0],arr[1]] satisfies Touple<number>;
+
+  allocator.free(ptr);
+  return touple;
+}
+
+
+
