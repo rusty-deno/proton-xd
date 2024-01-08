13f599b7db1be59a7f5fb7b998cb7ed8783b5c02
kakashi-69-xd <trishiragolder69@outlook.com>
2024-01-08 09:17:06 +0530

    improved Option macros

--- lib/std/error/option/macros.ts
+++ lib/std/error/option/macros.ts
@@ -1,9 +1,12 @@
+// deno-lint-ignore-file no-explicit-any
 import { Option,None } from "./option.ts";
+import { Fn,AsyncFn } from "../../types.ts";
+import { AsyncOption } from "./async_option.ts";
 
-export async function $option<T>(f: ()=> Promise<T|None>) {
-  return new Option<T>(await f());
+export function $option<T,P extends any[]>(f: AsyncFn<P,T|None>,args: P) {
+  return new AsyncOption<T>(f(...args).then(opt=> new Option(opt)));
 }
 
-export function $optionSync<T>(f: ()=> T|None) {
-  return new Option<T>(f());
+export function $optionSync<T,P extends any[]>(f: Fn<P,T|None>,...args: P) {
+  return new Option<T>(f(...args));
 }
