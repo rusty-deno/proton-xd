a861a903d374b7e6d41493f57532ed2f40262d15
kakashi-69-xd <trishiragolder69@outlook.com>
2024-01-08 09:09:23 +0530

    imrpoved Result macros

--- lib/std/error/result/macros.ts
+++ lib/std/error/result/macros.ts
@@ -1,14 +1,16 @@
+// deno-lint-ignore-file no-explicit-any
+import { Fn,AsyncFn } from "../../types.ts";
 import { Ok,Err } from "./result.ts";
 import { AsyncResult } from "./async_result.ts";
 
 
-export function $result<T,E=Err>(f: ()=> Promise<T>): AsyncResult<T,E> {
-  return new AsyncResult(f().then(res=> Ok(res)).catch(err=> Err(err)));
+export function $result<T,P extends any[],E=Err>(f: AsyncFn<P,T>,...args: P): AsyncResult<T,E> {
+  return new AsyncResult(f(...args).then(res=> Ok(res)).catch(err=> Err(err)));
 }
 
-export function $resultSync<T,E=Err>(f: ()=> T) {
+export function $resultSync<T,P extends any[],E=Err>(f: Fn<P,T>,...args: P) {
   try {
-    return Ok<T,E>(f());
+    return Ok<T,E>(f(...args));
   } catch (error) {
     return Err<T,E>(error);
   }
