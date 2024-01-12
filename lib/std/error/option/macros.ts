96d446962795e936c817d4fe6f1a8c7459ab55d6
kakashi-69-xd <trishiragolder69@outlook.com>
2024-01-12 11:33:39 +0530

    fixed macros

--- lib/std/error/option/macros.ts
+++ lib/std/error/option/macros.ts
@@ -3,7 +3,7 @@ import { Option,None } from "./option.ts";
 import { Fn,AsyncFn } from "../../types.ts";
 import { AsyncOption } from "./async_option.ts";
 
-export function $option<T,P extends any[]>(f: AsyncFn<P,T|None>,args: P) {
+export function $option<T,P extends any[]>(f: AsyncFn<P,T|None>,...args: P) {
   return new AsyncOption<T>(f(...args).then(opt=> new Option(opt)));
 }
 
